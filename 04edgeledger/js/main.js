13f82d52b0b7093a3d2edb424600c8bc2ea5a7d8
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-06-09 21:17:33 +0530

    Added Smooth Scrolling and Other Js

--- 04edgeledger/js/main.js
+++ 04edgeledger/js/main.js
@@ -5,3 +5,22 @@ function initMap() {
     zoom: 8
   });
 }
+window.addEventListener('scroll',function(){
+    if(this.window.scrollY > 150){
+        document.querySelector("#navbar").style.opacity=0.9;
+    }else{
+        document.querySelector("#navbar").style.opacity=1;
+    }
+});
+$("#navbar a,.btn").on("click",function(event){
+  event.preventDefault();
+  if(this.hash != ''){
+    const hash = this.hash;
+    //alert(hash);
+    $('html,body').animate({
+        scrollTop:$(hash).offset().top - 100
+    }, 800);
+    return false;
+  }
+   
+});
